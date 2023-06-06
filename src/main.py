bd6c2b7dd0d3ba8225258eccef55dfcc64cee817
DanesH-Abdollahi <danesh.abdollahii@gmail.com>
2023-06-06 23:51:52 +0330

    Add champion list calculations

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from math import log10
 normalizer = Normalizer()
 tokenizer = WordTokenizer()
 lemmetizer = Lemmatizer()
+sentence_tokenizer = SentenceTokenizer()
 stopwords = stopwords_list()
 stopwords.extend(
     [
@@ -56,7 +57,6 @@ with open("inverted_index.json", "r") as file:
 
 tokens_dict = dict()
 for id in data:
-    # tmp_tokens = []
     tmp = normalizer.normalize(data[id]["content"])  # normalize text
     tmp = tokenizer.tokenize(tmp)  # list of tokens
 
@@ -93,6 +93,20 @@ for token in inverted_index:
 
 # -------------------------------------------------------------------------------------------------
 
+# Add champion list to inverted index for each token
+r = 10  # Number of documents in champion list
+for token in inverted_index:
+    champion_list = []
+    for doc_id in inverted_index[token]:
+        if doc_id != "total_frequency" and doc_id != "champion_list":
+            champion_list.append((doc_id, inverted_index[token][doc_id]["tf-idf"]))
+
+    champion_list = sorted(champion_list, key=lambda x: x[1], reverse=True)
+    inverted_index[token]["champion_list"] = champion_list[:r]
+
+# -------------------------------------------------------------------------------------------------
+K = 5  # Number of documents to retrieve
+
 # Query Processing
 query = input("Enter your query: ")
 query = normalizer.normalize(query)
@@ -127,56 +141,200 @@ for token in query_vector:
 cosine_similarity = {}
 for token in query_vector:
     for doc_id in inverted_index[token]:
-        if doc_id != "total_frequency":
+        if doc_id != "total_frequency" and doc_id != "champion_list":
             tf_idf = inverted_index[token][doc_id]["tf-idf"]
             if doc_id not in cosine_similarity:
                 cosine_similarity[doc_id] = tf_idf * query_vector[token]
             else:
                 cosine_similarity[doc_id] += tf_idf * query_vector[token]
 
-for doc_id in cosine_similarity:
-    cosine_similarity[doc_id] /= document_norms[doc_id] ** 0.5
 
 # Sort documents based on cosine similarity
-ranked_result = sorted(cosine_similarity.items(), key=lambda x: x[1], reverse=True)
+cosine_similarity_results = sorted(
+    cosine_similarity.items(), key=lambda x: x[1], reverse=True
+)
 
-K = 5
-# print(f"Top {K} documents:")
-# for i in range(K):
+# -------------------------------------------------------------------------------------------------
+
+# Write top K results for cosine similarity in Results.txt
+results_for_show = cosine_similarity_results[:K]
+sentences = dict()
+for id, _ in results_for_show:
+    content = data[id]["content"]
+    content = normalizer.normalize(content)
+    sentences[id] = sentence_tokenizer.tokenize(content)
+    tmp = sentences[id].copy()
+    for sent in tmp:
+        tokens = tokenizer.tokenize(sent)
+        tokens = [lemmetizer.lemmatize(token) for token in tokens]
+
+        flag = 0
+        for query_token in query:
+            if query_token in tokens:
+                flag = 1
+                break
+
+        if flag == 0:
+            sentences[id].remove(sent)
+
+with open("Results.txt", "w") as file:
+    file.write("Cosine Similarity\n")
+    for id, _ in results_for_show:
+        file.write(f"Document ID: {id}\n")
+        file.write(f"Document Title: {data[id]['title']}\n")
+        file.write("Related Sentences:\n")
+        for sent in sentences[id]:
+            file.write(f"{sent}\n")
+        file.write("-" * 100 + "\n\n")
+
+# -------------------------------------------------------------------------------------------------
+
+# Calculate Cosine Similarity with champion list method
+cosine_similarity_champion_list = {}
+for token in query_vector:
+    for doc_id, tf_idf in inverted_index[token]["champion_list"]:
+        if doc_id not in cosine_similarity_champion_list:
+            cosine_similarity_champion_list[doc_id] = tf_idf * query_vector[token]
+        else:
+            cosine_similarity_champion_list[doc_id] += tf_idf * query_vector[token]
+
+
+# Sort documents based on cosine similarity for champion list
+cosine_similarity_champion_list_results = sorted(
+    cosine_similarity_champion_list.items(), key=lambda x: x[1], reverse=True
+)
+
+# -------------------------------------------------------------------------------------------------
+
+# Write top K results for cosine similarity with champion list in Results.txt
+results_for_show = cosine_similarity_champion_list_results[:K]
+sentences = dict()
+
+for id, _ in results_for_show:
+    content = data[id]["content"]
+    content = normalizer.normalize(content)
+    sentences[id] = sentence_tokenizer.tokenize(content)
+    tmp = sentences[id].copy()
+    for sent in tmp:
+        tokens = tokenizer.tokenize(sent)
+        tokens = [lemmetizer.lemmatize(token) for token in tokens]
+
+        flag = 0
+        for query_token in query:
+            if query_token in tokens:
+                flag = 1
+                break
+
+        if flag == 0:
+            sentences[id].remove(sent)
+
+with open("Results.txt", "a") as file:
+    file.write("Cosine Similarity with Champion List\n")
+    for id, _ in results_for_show:
+        file.write(f"Document ID: {id}\n")
+        file.write(f"Document Title: {data[id]['title']}\n")
+        file.write("Related Sentences:\n")
+        for sent in sentences[id]:
+            file.write(f"{sent}\n")
+        file.write("-" * 100 + "\n\n")
 
 # -------------------------------------------------------------------------------------------------
 
 # Calculate jaccard similarity using inverted index and data collection
-jaccard_similarity2 = {}
+jaccard_similarity = {}
 for token in query_vector:
     for doc_id in inverted_index[token]:
-        if doc_id != "total_frequency":
-            if doc_id not in jaccard_similarity2:
+        if doc_id != "total_frequency" and doc_id != "champion_list":
+            if doc_id not in jaccard_similarity:
                 intersection_length = len(tokens_dict[doc_id].intersection(set(query)))
                 union_length = len(tokens_dict[doc_id].union(set(query)))
-                jaccard_similarity2[doc_id] = intersection_length / union_length
+                jaccard_similarity[doc_id] = intersection_length / union_length
 
 # Sort documents based on jaccard similarity
-ranked_result = sorted(jaccard_similarity2.items(), key=lambda x: x[1], reverse=True)
-
-
-# Construct champion list for each token
-champion_list = []
-for token in inverted_index:
-    tmp = sorted(
-        inverted_index[token].items(), key=lambda x: x[1]["tf-idf"], reverse=True
-    )
-    champion_list.append((token, tmp[:100]))
+jaccard_similarity_result = sorted(
+    jaccard_similarity.items(), key=lambda x: x[1], reverse=True
+)
 
 # -------------------------------------------------------------------------------------------------
 
-# Add champion list to inverted index for each token
-r = 10
-for token in inverted_index:
-    champion_list = []
-    for doc_id in inverted_index[token]:
-        if doc_id != "total_frequency" and doc_id != "champion_list":
-            champion_list.append((doc_id, inverted_index[token][doc_id]["tf-idf"]))
+# Write top K results for jaccard similarity in Results.txt
+results_for_show = jaccard_similarity_result[:K]
+sentences = dict()
+for id, _ in results_for_show:
+    content = data[id]["content"]
+    content = normalizer.normalize(content)
+    sentences[id] = sentence_tokenizer.tokenize(content)
+    tmp = sentences[id].copy()
+    for sent in tmp:
+        tokens = tokenizer.tokenize(sent)
+        tokens = [lemmetizer.lemmatize(token) for token in tokens]
 
-    champion_list = sorted(champion_list, key=lambda x: x[1], reverse=True)
-    inverted_index[token]["champion_list"] = champion_list[:r]
+        flag = 0
+        for query_token in query:
+            if query_token in tokens:
+                flag = 1
+                break
+
+        if flag == 0:
+            sentences[id].remove(sent)
+
+with open("Results.txt", "a") as file:
+    file.write("Jaccard Similarity\n")
+    for id, _ in results_for_show:
+        file.write(f"Document ID: {id}\n")
+        file.write(f"Document Title: {data[id]['title']}\n")
+        file.write("Related Sentences:\n")
+        for sent in sentences[id]:
+            file.write(f"{sent}\n")
+        file.write("-" * 100 + "\n\n")
+
+# -------------------------------------------------------------------------------------------------
+
+# Calculate Jaccard Similarity with champion list method
+jaccard_similarity_champion_list = {}
+for token in query_vector:
+    for doc_id, _ in inverted_index[token]["champion_list"]:
+        if doc_id not in jaccard_similarity_champion_list:
+            intersection_length = len(tokens_dict[doc_id].intersection(set(query)))
+            union_length = len(tokens_dict[doc_id].union(set(query)))
+            jaccard_similarity_champion_list[doc_id] = (
+                intersection_length / union_length
+            )
+
+# Sort documents based on jaccard similarity for champion list
+jaccard_similarity_champion_list_results = sorted(
+    jaccard_similarity_champion_list.items(), key=lambda x: x[1], reverse=True
+)
+
+# -------------------------------------------------------------------------------------------------
+
+# Write top K results for jaccard similarity with champion list in Results.txt
+results_for_show = jaccard_similarity_champion_list[:K]
+sentences = dict()
+for id, _ in results_for_show:
+    content = data[id]["content"]
+    content = normalizer.normalize(content)
+    sentences[id] = sentence_tokenizer.tokenize(content)
+    tmp = sentences[id].copy()
+    for sent in tmp:
+        tokens = tokenizer.tokenize(sent)
+        tokens = [lemmetizer.lemmatize(token) for token in tokens]
+
+        flag = 0
+        for query_token in query:
+            if query_token in tokens:
+                flag = 1
+                break
+
+        if flag == 0:
+            sentences[id].remove(sent)
+
+with open("Results.txt", "a") as file:
+    file.write("Jaccard Similarity with Champion List\n")
+    for id, _ in results_for_show:
+        file.write(f"Document ID: {id}\n")
+        file.write(f"Document Title: {data[id]['title']}\n")
+        file.write("Related Sentences:\n")
+        for sent in sentences[id]:
+            file.write(f"{sent}\n")
+        file.write("-" * 100 + "\n\n")
